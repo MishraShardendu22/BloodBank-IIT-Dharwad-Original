31f7cb1d7500b8e06f287dc17880ff1cbab7f35b
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-01-26 02:25:47 +0530

    Refactor import comments for clarity in server index file

--- server/index.ts
+++ server/index.ts
@@ -22,11 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Import imports
+// Import
 
 import router from './route/route';
 
-// Import imports
+// Import
 
 // ----------------------------------------------- //
 
