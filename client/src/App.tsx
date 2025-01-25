84b89fc9eb1cd8845173552de33a206be1eaef62
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-01-26 03:05:22 +0530

    Add NotFound page with animations and routing; update App component to include new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./index.css"
+import Landing from "./Pages/Landing";
+import NotFound from "./Pages/NotFound";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
